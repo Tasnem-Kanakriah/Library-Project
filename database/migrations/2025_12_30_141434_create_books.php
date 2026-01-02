74f0044b5f6a4faa35d5a1ca5c68839d2a2d10f6
tasnem kanakriah <tasnemkanakriah9302@gmail.com>
2026-01-02 09:01:20 +0300

    make tabels for database (migrations)

---
+++ database/migrations/2025_12_30_141434_create_books.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->char("ISBN", 13);
+            $table->string('title', 50)->index();
+            $table->decimal("price", 4, 2)->default(0);
+            $table->decimal("mortgage", 6, 2)->comment('restored when returned');
+            $table->date("authorship_date")->nullable();
+            // $table->unsignedBigInteger('category_id');
+            $table->foreignId("category_id");
+            $table->foreign("category_id")->references("id")->on("categories");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
